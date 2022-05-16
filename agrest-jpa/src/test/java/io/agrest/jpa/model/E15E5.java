35f00e21a34c46e5ffcc72abf5432d86656bcf61
Ivan-nikitko <70625960+Ivan-nikitko@users.noreply.github.com>
2022-05-16 16:33:25 +0200

    add equals and hashCode to E15E5

--- agrest-jpa/src/test/java/io/agrest/jpa/model/E15E5.java
+++ agrest-jpa/src/test/java/io/agrest/jpa/model/E15E5.java
@@ -17,4 +17,21 @@ public class E15E5 implements Serializable {
     @Id
     private Long e15_id;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        E15E5 e15E5 = (E15E5) o;
+
+        if (e5_id != null ? !e5_id.equals(e15E5.e5_id) : e15E5.e5_id != null) return false;
+        return e15_id != null ? e15_id.equals(e15E5.e15_id) : e15E5.e15_id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = e5_id != null ? e5_id.hashCode() : 0;
+        result = 31 * result + (e15_id != null ? e15_id.hashCode() : 0);
+        return result;
+    }
 }
